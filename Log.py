df65ba41b9650b6db53586dc3b778d219d20800c
M02Mu <mohamed.muheisen@stud.fra-uas.de>
2024-05-17 16:51:42 +0200

    Methode Logeintrag

--- Log.py
+++ Log.py
@@ -4,3 +4,8 @@ class log:
         self.now = datetime.now()
         self.date = self.now.strftime("%d/%m/%Y %H:%M:%S")
         pass
+
+    def logEintrag(self):
+        self.now = datetime.now()
+        with open('\M02Mu\PycharmProjects\BSRN-Projekt\Protokoll', 'a') as file:
+            file.write(self.date)
