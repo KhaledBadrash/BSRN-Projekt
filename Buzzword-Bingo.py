86fdc6f2d3b7857189ced218d8b9d9646f86b289
khaled <khaled.badrash@stud.fra-uas.de>
2024-05-17 16:21:32 +0200

    auskommentierung

--- Buzzword-Bingo.py
+++ Buzzword-Bingo.py
@@ -1,13 +1,11 @@
 import random
 
-
-def buzzwords():
-    w = random.randint(0, 49)  #Zufallszahl-Generator
-    print(w)
-    with open("Textdatei", "r") as file:  #Textdatei aus Github
-        content = file.readlines()  #Datei lesen
-    return content[w]  # Wort aus einer zufälligen Zeile, bestimmt vom Generator
-
-
-for buzzword in range(25): #zum Karten erstellen (test) / Es wird genau 25 mal ramdom ein Wort aus der Textdatei geprintet
-    print(buzzwords())
+#Auskommentiert bis vorarbeit geleistet
+#def buzzwords():
+#    w = random.randint(0, 49)  #Zufallszahl-Generator
+#   print(w)
+#   with open("Textdatei", "r") as file:  #Textdatei aus Github
+#       content = file.readlines()  #Datei lesen
+#   return content[w]  # Wort aus einer zufälligen Zeile, bestimmt vom Generator
+#for buzzword in range(25): #zum Karten erstellen (test) / Es wird genau 25 mal ramdom ein Wort aus der Textdatei geprintet
+#   print(buzzwords())
