f74dc6797a15e935fdc51b1261d56512ce4cc136
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-10 15:47:11 +0200

    app

--- bingoApp.py
+++ bingoApp.py
@@ -1,12 +1,33 @@
-from textual.app import App
+from textual.app import App, ComposeResult
+from textual.widgets import Static, Button
 
-class bingoApp(App):
-    pass
+class TestButton(Static):
+    def compose(self):
+    #def for schleife für bestimmung der breite
+        for i in range(0,5): #Feldgröße wird vom scanner aus der test klasse übernommen
+            yield Button("Welcome") #wörter aus txt doc ramdom zugewiesen ohne wiederholung
 
-    # def for schleife für bestimmung der laenge
 
-        for i in range(0, 5):
-            yield TestButton()
+class WidgetApp(App):
+    def compose(self) -> ComposeResult:
+        self.widget = Static("Buzzword Bingo")
+        yield self.widget
 
-if __name__ == '__main__':
-    bingoApp().run()
+
+    CSS = """
+    TestButton {
+        layout: horizontal;
+    }
+    """
+    #Schleife while schleife für spieler
+    #Felder auswählen --> Effekt
+    #Stritt zurück
+    def on_mount(self) -> None:
+        self.widget.styles.background = "aquamarine"
+        self.widget.styles.color = "black"
+        self.widget.styles.border = ("heavy", "black")
+        self.widget.styles.width = 17 #manueel mit
+
+if __name__ == "__main__":
+    app = WidgetApp()
+    app.run()
