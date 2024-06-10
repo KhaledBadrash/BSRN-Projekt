befe10ecc59a725035f0ed51983bcc45dddc21c4
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-10 18:14:56 +0200

    kleine verbesserungen

--- Buzzword-Bingo.py
+++ Buzzword-Bingo.py
@@ -10,6 +10,7 @@ startlog.logeintragstart()
 bspLog = Log.log()
 bspLog.logeintragstep()
 
+anz_spieler = 0
 #Beginn des Spieles
 print("Willkommen im Buzzword-Bingospiel.")
 
@@ -19,16 +20,18 @@ if variable == "Y" or variable == "y":
     anz_spieler = int(input("Geben Sie die Anzahl der Spieler an:"))
     feld_gr = int(input("Geben Sie die Größe ihres Spielfeldes: \n 1 = 3X3 \n 2 = 5X5 \n 3 = 7X7"))
 
-elif variable == "N" or variable == "n": #elif else if
+elif variable == "N" or variable == "n":  #elif else if
     print("Das Spiel ist beendet")
 
     #Beginn von der Interprosesskomunikation
 processes_spieler = []
 #if abfrage für geldgr 1,2,3,4 also 5 möglichkeiten
-num_words = 25 #testvariable für 5x5  # Beispiel: 25 zufällige Wörter
+num_words = 25  #testvariable für 5x5  # Beispiel: 25 zufällige Wörter
 
 # Pfad zur Datei und Anzahl der gewünschten Wörter
-file_url = input("Geben Sie den gewünschten Pfad an -wordfile: ")#https://raw.githubusercontent.com/KhaledBadrash/BSRN-Projekt/main/Textdatei
+file_url = input(
+    "Geben Sie den gewünschten Pfad an -wordfile: ")  #https://raw.githubusercontent.com/KhaledBadrash/BSRN-Projekt/main/Textdatei
+
 
 def bingo_cards(url, anz_woerter):
     # Wörter aus der Datei lesen
@@ -50,6 +53,3 @@ for _ in range(anz_spieler):
     p.start()
     processes_spieler.append(p)
     print("Spieler ", _ + 1, " hat das Spiel beigetreten: ", {p.pid})
-
-
-
