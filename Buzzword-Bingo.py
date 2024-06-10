d0e782b57009f4b7fe97ced8baa7f18942d1568a
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-10 15:39:11 +0200

    xx

--- Buzzword-Bingo.py
+++ Buzzword-Bingo.py
@@ -3,29 +3,20 @@ import requests
 import random
 import multiprocessing as mp
 
-print('Herlich Willkommen zu Buzzword-Bingo!')
-eingabeBreite = int(input('Geben Sie die Breite des Spielfeldes an -xaxis: '))
-eingabeHoehe = int(input('Geben Sie die Hoehe des Spielfeldes an -yaxis:  '))
-
 #Falls Eingabe von Hoehe und Breite nicht gleich ist
-if eingabeHoehe != eingabeBreite:
-    print('Beide Eingaben muessen gleich sein!')
-
-if eingabeHoehe == eingabeBreite:
-    print()
 
 startlog = Log.log()
 startlog.logeintragstart()
 bspLog = Log.log()
 bspLog.logeintragstep()
 
-anz_spieler = 0
+anz_spieler = 3
 feld_gr = 0
 processes_spieler = []
 num_words = 25  # Beispiel: 25 zufällige Wörter
 
 # Pfad zur Datei und Anzahl der gewünschten Wörter
-file_url = 'https://raw.githubusercontent.com/KhaledBadrash/BSRN-Projekt/main/Textdatei'
+file_url = input("-wordfile: ")
 
 def bingo_cards(url, anz_woerter):
     # Wörter aus der Datei lesen
