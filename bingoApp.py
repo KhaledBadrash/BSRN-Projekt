8ba9e8715afe6a5808cadfb6c1a37c248cddfd50
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-10 15:41:57 +0200

    xx

--- bingoApp.py
+++ bingoApp.py
@@ -3,5 +3,10 @@ from textual.app import App
 class bingoApp(App):
     pass
 
+    # def for schleife für bestimmung der laenge
+
+        for i in range(0, 5):
+            yield TestButton()
+
 if __name__ == '__main__':
     bingoApp().run()
