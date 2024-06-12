e78c6becf9e2e614e9699701ae19b171e6a4e041
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-12 18:04:03 +0200

    verbesserungen ..

--- ipc.py
+++ ipc.py
@@ -98,50 +98,21 @@ if __name__ == "__main__":  #m
     wortdatei = input("Pfad zur Wortdatei: (z.B.Woerter) ")  # Fragt den Pfad zur txt.datei ab k
 
     print("Willkommen im Buzzword-Bingospiel.")
-        #Abfrage
-        variable = input("Neue Runde? \n [Y/N]: ")
-        if variable == "Y" or variable == "y":
+    #Abfrage
+    variable = input("Neue Runde? \n [Y/N]: ")
+    if variable == "Y" or variable == "y":
+        # x und y achse TBD
+        xachse = int(input("Anzahl der Felder in der Breite: "))  # Fragt die Anzahl der Felder in der Breite ab
+        yachse = int(input("Anzahl der Felder in der Höhe: "))  # Fragt die Anzahl der Felder in der Höhe ab
+        spieler_name = input("Name des Spielers: ")  # Fragt den Namen des Spielers ab --- PID TBD
+    elif variable == "N" or variable == "n":  # elif else if
+            print("Das Spiel ist beendet")
 
+# Erstelle die Pipe
+    empf_pipe, sender_pipe = os.pipe()  # Erstellt ein anonymes Pipe-Paar für die Kommunikation #k
 
-#Falls Eingabe von Hoehe und Breite nicht gleich ist
-
-startlog = Log.log()
-startlog.logeintragstart()
-bspLog = Log.log()
-bspLog.logeintragstep()
-
-anz_spieler = 0
-#Beginn des Spieles
-print("Willkommen im Buzzword-Bingospiel.")
-
-#Abfrage
-variable = input("Neue Runde? \n [Y/N]: ")
-if variable == "Y" or variable == "y":
-    anz_spieler = int(input("Geben Sie die Anzahl der Spieler an:"))
-    feld_gr = int(input("Geben Sie die Größe ihres Spielfeldes: \n 1 = 3X3 \n 2 = 5X5 \n 3 = 7X7"))
-
-elif variable == "N" or variable == "n":  #elif else if
-    print("Das Spiel ist beendet")
-
-    #Beginn von der Interprosesskomunikation
-processes_spieler = []
-#if abfrage für geldgr 1,2,3,4 also 5 möglichkeiten
-num_words = 25  #testvariable für 5x5  # Beispiel: 25 zufällige Wörter
-
-# Pfad zur Datei und Anzahl der gewünschten Wörter
-file_url = input(
-    "Geben Sie den gewünschten Pfad an -wordfile: ")  #https://raw.githubusercontent.com/KhaledBadrash/BSRN-Projekt/main/Textdatei
-
-
-def bingo_cards(url, anz_woerter):
-    # Wörter aus der Datei lesen
-    response = requests.get(url)
-    words = response.text.splitlines()
-
-    # Zufällige Wörter auswählen, ohne Duplikate
-    random_words = random.sample(words, anz_woerter)
-    return random_words
-
-
-unique_words = bingo_cards(file_url, num_words)
-print("Zufällige Wörter:", unique_words)
+    # Initialisiert das Spiel und startet die IPC
+    spiel = BingoSpiel(wortdatei, xachse, yachse, spieler_name, empf_pipe,
+                       sender_pipe)  # Initialisiert ein neues BingoSpiel-Objekt
+    spiel.starte_spiel()  # Startet das Spiel
+    spiel.starte_ipc()  # Startet die Interprozesskommunikation
