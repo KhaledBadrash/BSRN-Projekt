1b2424bf93b38d1af1818240b1d7b8a7d8cc8f45
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-12 17:38:04 +0200

    kleine verbesserungen

--- Buzzword-Bingo.py
+++ Buzzword-Bingo.py
@@ -1,32 +1,29 @@
-import Log
+#import Log
 import requests
 import random
-import multiprocessing as mp
 
-#Falls Eingabe von Hoehe und Breite nicht gleich ist
+# Falls Eingabe von Hoehe und Breite nicht gleich ist
 
 startlog = Log.log()
 startlog.logeintragstart()
 bspLog = Log.log()
 bspLog.logeintragstep()
-
 anz_spieler = 0
-#Beginn des Spieles
+# Beginn des Spieles
 print("Willkommen im Buzzword-Bingospiel.")
-
-#Abfrage
+spieler = [len(anz_spieler)]
+# Abfrage
 variable = input("Neue Runde? \n [Y/N]: ")
 if variable == "Y" or variable == "y":
     anz_spieler = int(input("Geben Sie die Anzahl der Spieler an:"))
     feld_gr = int(input("Geben Sie die Größe ihres Spielfeldes: \n 1 = 3X3 \n 2 = 5X5 \n 3 = 7X7"))
 
-elif variable == "N" or variable == "n":  #elif else if
+elif variable == "N" or variable == "n":  # elif else if
     print("Das Spiel ist beendet")
 
-    #Beginn von der Interprosesskomunikation
-processes_spieler = []
-#if abfrage für geldgr 1,2,3,4 also 5 möglichkeiten
-num_words = 25  #testvariable für 5x5  # Beispiel: 25 zufällige Wörter
+    # Beginn von der Interprosesskomunikation
+    # if abfrage für geldgr 1,2,3,4 also 5 möglichkeiten
+num_words = 25  # testvariable für 5x5 # Beispiel: 25 zufällige Wörter
 
 # Pfad zur Datei und Anzahl der gewünschten Wörter
 file_url = input(
@@ -43,13 +40,8 @@ def bingo_cards(url, anz_woerter):
     return random_words
 
 
-unique_words = bingo_cards(file_url, num_words)
-print("Zufällige Wörter:", unique_words)
+spieler[0] = bingo_cards(file_url, anz_spieler)
 
-for _ in range(anz_spieler):
-    #erstelle Karte
-    #IPC pro Spieler ein Prozess
-    p = mp.Process(target=bingo_cards, args=(file_url, num_words))
-    p.start()
-    processes_spieler.append(p)
-    print("Spieler ", _ + 1, " hat das Spiel beigetreten: ", {p.pid})
+print("Zufällige Wörter:", spieler[0])
+
+# Initialisiere die Liste mit leeren Listen für jeden Spieler
