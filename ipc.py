32a3c8a17b529b39e7ec45d9806d1fb5d495e256
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-14 17:19:33 +0200

    verbesserungen

--- ipc.py
+++ ipc.py
@@ -53,7 +53,7 @@ class BingoSpiel:  #k
 
         print(
             f"Spieler {self.spieler_name} hat das Spiel gestartet.")  # Gibt eine Meldung aus, dass das Spiel gestartet wurde
-        self.logger.logeintrag({"Spieler: ", self.spieler_name, "hat als Host das Spiel gestartet"})
+        self.logger.logeintrag(f"Spieler: {self.spieler_name} hat als Host das Spiel gestartet")
 
     # Markiert ein Feld auf der Bingokarte
     def markiere_feld(self, x, y):  #TBD pipe problem sender und empfänger pipe muss noch hinzugefügt werden-k
