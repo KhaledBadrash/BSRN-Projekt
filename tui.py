b144e71af479e7677d402c9366dfa4d27a728225
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-14 15:26:02 +0200

    w

--- tui.py
+++ tui.py
@@ -6,9 +6,9 @@ import textual.css as css
 
 
 # Definiert eine Klasse für TestButtons, die auf der Bingokarte angezeigt werden
-class TestButton(Static):
+class BButton(Static):
 
-    def compose(self) -> ComposeResult:
+    def compose(self) -> ComposeResult: #TBD:   Wörter zuweisung + Layout
         yield Button("Test")
         yield Button("Test")
         yield Button("Test")
@@ -19,11 +19,10 @@ class TestButton(Static):
         yield Button("Test")
 
     def on_mount(self) -> None:
-        self.widget.styles.background = "aquamarine"
-        self.widget.styles.color = "black"
-        self.widget.styles.border = ("heavy", "black")
-        self.widget.styles.width = 30  #manueel mit
-
+        self.styles.background = "aquamarine"
+        self.styles.color = "black"
+        (self.styles).border = ("heavy", "black")
+        self.styles.width = 30  #manueel mit
 
 
 # Definiert die Haupt-App-Klasse für das Bingospiel
@@ -32,10 +31,10 @@ class WidgetApp(App):
     def compose(self) -> ComposeResult:
         self.widget = Static("Buzzword Bingo")
         yield self.widget
-        yield TestButton()
+        yield BButton()
         yield Grid(id="grid")  # Yield the Grid widget first
 
-    def on_mount(self) -> None:
+    def on_mount(self) -> None: #design der
         self.widget.styles.background = "aquamarine"
         self.widget.styles.color = "black"
         self.widget.styles.border = ("heavy", "black")
