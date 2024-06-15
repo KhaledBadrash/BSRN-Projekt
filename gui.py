b09ea7070108fd9dc4ab90b51e15a6a420ff1117
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-15 15:53:03 +0200

    NEUE GUI

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ gridLayout = ttk.TTkGridLayout(columnMinHeight=0, columnMinWidth=0)
 root = ttk.TTk(layout=gridLayout)
 
 original_texts = {}  #speichert die Texte pro button
-groesse_Feld = 3
+groesse_Feld = 3  # bestimmt Größe des Bingofeldes
 
 
 def clicker(button, original_text):  #streicht den Text mit X-Symbol und setzt zurück auf wiederholten Knopfdruck
@@ -27,15 +27,15 @@ def clicker(button, original_text):  #streicht den Text mit X-Symbol und setzt z
 for i in range(groesse_Feld):
     for j in range(groesse_Feld):
         if i == groesse_Feld / 2 + 0.5 - 1 and j == groesse_Feld / 2 + 0.5 - 1:  #prüft ob Joker erstellt werden muss
-                                                                                 #wenn ja, setzt ihn in Mitte
+                                                                                #wenn ja, setzt ihn in Mitte
             button = ttk.TTkButton(parent=root, border=True, text="X")  # erstellt Joker-Button
             original_texts[button] = button.text()
-            gridLayout.addWidget(button, i, j) #setzt button auf Position i und j, für Höhe u. Länge
+            gridLayout.addWidget(button, i, j)  #setzt button auf Position i und j, für Höhe u. Länge
         else:
-            button = ttk.TTkButton(parent=root, border=True, text="Button1") #erstellt normalen button
+            button = ttk.TTkButton(parent=root, border=True, text="Button1")  #erstellt normalen button
             original_texts[button] = button.text()
             gridLayout.addWidget(button, i, j)
-            button.clicked.connect(clicker(button, original_texts[button])) #Beim Anklicken führt er erstellte Klick-Methode aus
+            button.clicked.connect(
+                clicker(button, original_texts[button]))  #Beim Anklicken führt er erstellte Klick-Methode aus
 
-# Start the main event loop
-root.mainloop()
+root.mainloop()  #startet die App
