bb6e0024b7e26f6d87d604da62e4d5d882df9dc5
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-15 23:50:53 +0200

    Buzzwords bereitgestellt für Buttons

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import random
 import TermTk as ttk
 from TermTk.TTkCore.signal import pyTTkSignal
 from TermTk.TTkCore.cfg import TTkCfg
@@ -11,7 +12,12 @@ gridLayout = ttk.TTkGridLayout(columnMinHeight=0, columnMinWidth=0)
 root = ttk.TTk(layout=gridLayout)
 
 original_texts = {}  #speichert die Texte pro button
-groesse_Feld = 3  # bestimmt Größe des Bingofeldes
+groesse_Feld = 5  # bestimmt Größe des Bingofeldes
+
+file_path = "/mnt/c/Users/M02Mu/PycharmProjects/BSRN-Projekt/Textdatei"  #Textdatei für Bingofelder
+
+with open(file_path, 'r') as file:
+    words = file.read().splitlines()  #liest den Text zeilenweise
 
 
 def clicker(button, original_text):  #streicht den Text mit X-Symbol und setzt zurück auf wiederholten Knopfdruck
@@ -27,15 +33,21 @@ def clicker(button, original_text):  #streicht den Text mit X-Symbol und setzt z
 for i in range(groesse_Feld):
     for j in range(groesse_Feld):
         if i == groesse_Feld / 2 + 0.5 - 1 and j == groesse_Feld / 2 + 0.5 - 1:  #prüft ob Joker erstellt werden muss
-                                                                                #wenn ja, setzt ihn in Mitte
+            #wenn ja, setzt ihn in Mitte
             button = ttk.TTkButton(parent=root, border=True, text="X")  # erstellt Joker-Button
             original_texts[button] = button.text()
             gridLayout.addWidget(button, i, j)  #setzt button auf Position i und j, für Höhe u. Länge
         else:
-            button = ttk.TTkButton(parent=root, border=True, text="Button1")  #erstellt normalen button
+            text = random.choice(words)  #wählt eine zufällige Zeile und stellt das Wort in Zeile 42 zur Verfügung
+            button = ttk.TTkButton(parent=root, border=True, text=text)  #erstellt normalen button
             original_texts[button] = button.text()
             gridLayout.addWidget(button, i, j)
             button.clicked.connect(
                 clicker(button, original_texts[button]))  #Beim Anklicken führt er erstellte Klick-Methode aus
 
+bingo_check = true
+
+while bingo_check:  #Versuch Schleife zur Bingo-Überprüfung zu erstellen. TBD
+    if button.text == "X":
+        bingo_check = False
 root.mainloop()  #startet die App
