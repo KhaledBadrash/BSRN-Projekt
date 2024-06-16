9391e405e50fd87d24d85942c2af8e5d984ede2d
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-16 13:59:09 +0200

    changes

--- Khaled/args.py
+++ Khaled/args.py
@@ -2,7 +2,12 @@ from argparse import ArgumentParser, Namespace
 
 parser = ArgumentParser()
 
-parser.add_argument('eingabe')
-
+parser.add_argument('hochzwei',help='hoch2',type=int)
+parser.add_argument('-v','--verbose',help='verbose',action='store_true',required=True)
+#-v gleiche wie --verbose
 args: Namespace = parser.parse_args()
-print(args.eingabe)
+
+if args.verbose:
+    print(f'{args.hochzwei} hochzwei ist: {args.hochzwei ** 2}')
+else:
+    print(args.hochzwei ** 2)
