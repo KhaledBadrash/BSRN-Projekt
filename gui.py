4e0b34c8fe08bd69a95bc37caec25dc6a8516929
MohamedMuheisen <mohamed.muheisen@stud.fra-uas.de>
2024-06-17 17:15:04 +0200

    Kommentare teamwork

--- gui.py
+++ gui.py
@@ -1,41 +1,42 @@
-import random #
-import TermTk as ttk
+import random #zufalls...
+import TermTk as ttk #gui as -- ist für abkürzung
 from TermTk.TTkCore.signal import pyTTkSignal
 from TermTk.TTkCore.cfg import TTkCfg
 from TermTk.TTkCore.constant import TTkK
 from TermTk.TTkCore.string import TTkString
 from TermTk.TTkCore.signal import pyTTkSignal
-from TermTk.TTkWidgets.widget import TTkWidget
+from TermTk.TTkWidgets.widget import TTkWidget#TBD
 
-gridLayout = ttk.TTkGridLayout(columnMinHeight=0, columnMinWidth=0)
-root = ttk.TTk(layout=gridLayout)
+gridLayout = ttk.TTkGridLayout(columnMinHeight=0, columnMinWidth=0) #um das feld anziegen zu lassen und die Formatierung
+root = ttk.TTk(layout=gridLayout)   #root --> APP / 'Mainbenutzer'?                   #Column? --> Notwendig?
 
-original_texts = {}  #speichert die Texte pro button
-groesse_Feld = 5  # bestimmt Größe des Bingofeldes
+original_texts = {}  #speichert die Texte pro button /--TBD
+groesse_Feld = 8  # bestimmt Größe des Bingofeldes  /X-Y rausgehalten --muss mit args noch ..
 
 file_path = "/mnt/c/Users/M02Mu/PycharmProjects/BSRN-Projekt/Textdatei"  #Textdatei für Bingofelder
-
+                                                                        #Args uebergabe
 with open(file_path, 'r') as file:
     words = file.read().splitlines()  #liest den Text zeilenweise
 
 
-def clicker(button, original_text):  #streicht den Text mit X-Symbol und setzt zurück auf wiederholten Knopfdruck
+def klicker(button, original_text):  #streicht den Text mit X-Symbol und setzt zurück auf wiederholten Knopfdruck
+                                    #nur max 2 moves TBD
     def auf_knopfdruck():
-        if button.text() == "X":
-            button.setText(original_text)  #setzt den Text auf Buzzword zurück, wenn es markierd war
+        if button.text() == "X":            #Wenn text X
+            button.setText(original_text)   #setzt den Text auf Buzzword zurück, wenn es markierd war
         else:
-            button.setText("X")
+            button.setText("X")             #wenn nicht x dann settet es zu X
 
-    return auf_knopfdruck
+    return auf_knopfdruck   #returnt die Methode
 
 
-for i in range(groesse_Feld):
+for i in range(groesse_Feld):       #IF für Joker
     for j in range(groesse_Feld):
-        if i == groesse_Feld / 2 + 0.5 - 1 and j == groesse_Feld / 2 + 0.5 - 1:  #prüft ob Joker erstellt werden muss
+        if i == groesse_Feld / 2 + 0.5 - 1 and j == groesse_Feld / 2 + 0.5 - 1 and i != 2:  #prüft ob Joker erstellt werden muss
             #wenn ja, setzt ihn in Mitte
             button = ttk.TTkButton(parent=root, border=True, text="X")  # erstellt Joker-Button
             original_texts[button] = button.text()
-            gridLayout.addWidget(button, i, j)  #setzt button auf Position i und j, für Höhe u. Länge
+            gridLayout.addWidget(button, i, j)  #setzt button auf Position i/X und j/Y, für Höhe u. Länge
         else:
             text = random.choice(words)  #wählt eine zufällige Zeile und stellt das Wort in Zeile 42 zur Verfügung
             button = ttk.TTkButton(parent=root, border=True, text=text)  #erstellt normalen button
