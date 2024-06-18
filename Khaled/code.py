48df74620c8c272e3491d69c25811df1a57c4d89
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-18 21:12:31 +0200

    Implemet log and JSON

--- Khaled/code.py
+++ Khaled/code.py
@@ -5,17 +5,22 @@ import json
 from datetime import datetime
 
 # Hilfsfunktion, um Daten in eine JSON-Datei zu loggen
-def log_data(button_text, x_value, y_value, work_date, personal_name):
+
+
+def log_data(host_name,button_text, x_wert, y_wert, auswahl_zeitpunkt):
     data = {
+        'host_name': host_name,
         'button_text': button_text,
-        'x_value': x_value,
-        'y_value': y_value,
-        'work_date': work_date,
-        'personal_name': personal_name
+        'x_wert': x_wert,
+        'y_wert': y_wert,
+        'auswahl_zeitpunkt': auswahl_zeitpunkt
+
     }
+
     with open('log_data_host.json', 'a') as file:  # 'a' um Daten an die Datei anzuhängen
         json.dump(data, file)
         file.write('\n')  # Neue Zeile für bessere Lesbarkeit in der Datei
+
 
 def lade_woerter(woerter_pfad, xachse, yachse):
     try:
@@ -31,10 +36,12 @@ def lade_woerter(woerter_pfad, xachse, yachse):
     except ValueError as e:
         return str(e)
 
+
 def gewinner_screen(parent):
     win_root = ttk.TTkWindow(parent=parent, title="Gewinner", border=True, pos=(35, 5), size=(30, 10))
     ttk.TTkLabel(parent=win_root, text="Gewinner! Herzlichen Glückwunsch!", pos=(2, 2))
     win_root.raiseWidget()
+
 
 def main(args):
     grid_layout = ttk.TTkGridLayout(columnMinHeight=0, columnMinWidth=0)
@@ -58,7 +65,7 @@ def main(args):
                 button.setText("X")
                 klick_counter[0] += 1  # Klickzähler erhöhen
 
-                log_data(str(original_text), x, y, datetime.now().strftime('%d-%m-%Y %H:%M:%S'), args.personal_name)
+                log_data(args.personal_name, str(original_text), x, y, datetime.now().strftime('%d-%m-%Y %H:%M:%S Uhr'))
                 if klick_counter[0] == 3:
                     gewinner_screen(root)
         return auf_knopfdruck
@@ -83,6 +90,7 @@ def main(args):
 
     root.update()
     root.mainloop()
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
