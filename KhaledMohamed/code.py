6e557a893b0b8a3ab9a28975f6bd20868df24a17
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-19 21:08:11 +0200

    ...

--- KhaledMohamed/code.py
+++ KhaledMohamed/code.py
@@ -138,6 +138,15 @@ def main(args):
             if all((j, i) in marked_positions for j in range(max_feld)):
                 return True
 
+            # Check diagonal lines (top-left to bottom-right)
+            if all((j, j) in marked_positions for j in range(max_feld)):
+                return True
+
+            # Check diagonal lines (top-right to bottom-left)
+            if all((j, max_feld - 1 - j) in marked_positions for j in range(max_feld)):
+                return True
+
+    return False
     def klicker(button, original_text, x, y):
         def auf_knopfdruck():
             logs = read_json_log()
