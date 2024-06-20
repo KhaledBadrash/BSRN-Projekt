db453905d10fcefcff6255c5126480dc23925e3b
rajan <rajan.kakkar@stud.fra-uas.de>
2024-06-20 14:27:11 +0200

    ...

--- Rajan/pipes.py
+++ Rajan/pipes.py
@@ -4,21 +4,17 @@ from datetime import datetime
 
 from codeR import host_log_data, log_game_start, log_win, pipe_path
 
-pipe_path = 'pipe'  # Assuming the pipe is created elsewhere (or modify the path if needed)
-
 
 def read_pipe():
-    pipe_path = 'pipes.py'  # Assuming the pipe is created elsewhere
-
-    # Try creating the pipe if it doesn't exist
+    pipe_path = 'pipes.py'
     try:
         with open(pipe_path, 'r') as pipe:  # Open for reading
-            data = json.load(pipe)  # Attempt to read JSON
+            data = json.load(pipe)
             return data
     except FileNotFoundError:
-        # Create the pipe if it's missing
+
         os.mkfifo(pipe_path)
-        return None  # Indicate no data available
+        return None
 
 def write_pipe(data):
     with open(pipe_path, 'w') as pipe:
@@ -27,7 +23,7 @@ def write_pipe(data):
 def main():
     while True:
         data = read_pipe()
-        if data:  # Check if data was read successfully
+        if data:
             if data['action'] == 'log_data':
                 host_log_data(data['host_name'], data['button_text'],
                               data['x_wert'], data['y_wert'], data['auswahl_zeitpunkt'])
