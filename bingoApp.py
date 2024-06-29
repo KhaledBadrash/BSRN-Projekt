954665157dfec90bd6afd7ca47ff32c165417c81
KhaledBadrash <168454366+KhaledBadrash@users.noreply.github.com>
2024-06-29 18:15:32 +0200

    FERTIG:)

--- bingoApp.py
+++
@@ -1,9 +0,0 @@
-# Check diagonal lines (top-left to bottom-right)
-if all((j, j) in marked_positions for j in range(max_feld)):
-    return True
-
-# Check diagonal lines (top-right to bottom-left)
-if all((j, max_feld - 1 - j) in marked_positions for j in range(max_feld)):
-    return True
-
-return False
